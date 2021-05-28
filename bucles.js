49d48a3c0af97a1c8ce63f9ade00bb9b0bba647f
LuisMeneses <luis.meneses@gmail.com>
2021-05-27 22:52:05 -0700

    second part of 1/100

---
+++ bucles.js
@@ -0,0 +1,12 @@
+//Bucles
+
+let names = ['Luis','Julian', 'Miguel'];
+
+console.log("First second: "+names[1]);
+console.log("Array lenght: "+names.length);
+
+console.log("------ FOR -------");
+for(let i = 0; i< names.length ; i++){
+    console.log("Name: "+names[i]);
+}
+console.log("------ ENDFOR -------");
---
+++ function.js
@@ -0,0 +1,25 @@
+//Function
+
+console.log("------ FUNCTION -------"+'\n');
+
+//Create
+function greeting(){
+    console.log("Hello")
+}
+//Call
+greeting();
+
+function name(name){
+    console.log(name);
+}
+name("Luis");
+
+//Add two number
+function add(numberOne, numbertwo){
+    let result= numberOne+numbertwo;
+    console.log(result);
+}
+add(14,20);
+document.write("Hello World");
+
+// DOCUMENT OBJECT MODEL
